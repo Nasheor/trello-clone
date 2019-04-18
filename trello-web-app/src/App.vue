5973108b18fbd014018152517b9e57633298d90d
Nasheor <nash.avinashnagarajan@gmail.com>
2019-04-18 11:14:35 +0100

    Built starter code

--- trello-web-app/src/App.vue
+++ trello-web-app/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -13,5 +9,9 @@
 
 #app {
   background-color: #42f4ee;
+}
+
+.container {
+  @apply mx-auto;
 }
 </style>
--- trello-web-app/src/router.js
+++ trello-web-app/src/router.js
@@ -1,8 +1,18 @@
+/* eslint-disable prettier/prettier */
 import Vue from "vue";
 import Router from "vue-router";
+import Board from "./views/Board";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'board',
+      component: Board
+    }
+  ]
 });
